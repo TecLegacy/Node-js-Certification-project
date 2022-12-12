0cfe4f067bcab0f6b2f744c9880ff1d66dd0e6f7
TecLegacy <99929931+TecLegacy@users.noreply.github.com>
2022-12-12 08:59:44 +0530

    Cart Logic

--- model/cartModel.js
+++ model/cartModel.js
@@ -1,27 +1,49 @@
-/** Cart Stucture
- * cart [{product:[{id:1,quantity:2}],totalPrice:'tp+quantity*idprice'}]
+/**
+ *  cart [ products: [] ,totalPrice : ' ']  -> product { id, qnty }
  */
 
+const fs = require('fs');
 const path = require('path');
-const fs = requir('fs');
 
 const root = require('../utils/parentDirectory');
 
+//Path to cartfile
 const p = path.join(root, 'data', 'cart.json');
 
 module.exports = class Cart {
-  static getCart(id) {
-    let cart = {
-      product: [],
-      totalPrice: '',
-    };
-    //Read -> Append/Replace -> Write in cart
+  static addCart(id, articlePrice) {
+    //Check for exisiting data in cart from FS
     fs.readFile(p, (err, fileContent) => {
+      let cart = {
+        products: [],
+        totalPrice: '',
+      };
       if (!err) {
         cart = JSON.parse(fileContent);
       }
-      //Cart Exists
-      const cartExists = cart.product.findIndex(curr => id === curr.id);
+
+      const cartExistIndex = cart.product.findIndex(curr => curr.id === id);
+      const cartExist = cart.product[cartExistIndex];
+      let updatedCart;
+
+      //Increase quantity by 1
+      if (cartExist) {
+        updatedCart = { ...cartExist };
+        updatedCart.qnty += 1;
+
+        cart.products = [...cart.products]; // <- WHY ? is this needed
+        cart.products[cartExistIndex] = updatedCart;
+      } else {
+        updatedCart = {
+          id,
+          qnty: 1,
+        };
+        cart.products = [...cart.products, updatedCart];
+      }
+      cart.totalPrice += articlePrice;
+
+      //save cart back to FS
+      fs.writeFile(p, JSON);
     });
   }
 };
